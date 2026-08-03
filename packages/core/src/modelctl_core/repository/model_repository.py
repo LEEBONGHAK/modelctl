d0b6b83fc60e81cba01b53e9bc959ebfced63b77
bolee <dlqhdgkr456@gmail.com>
2026-08-03 10:54:22 +0900

    fix: resolve repository lint errors

--- packages/core/src/modelctl_core/repository/model_repository.py
+++ packages/core/src/modelctl_core/repository/model_repository.py
@@ -1,24 +1,15 @@
 from datetime import UTC, datetime
 
-from sqlmodel import Session
-from sqlmodel import select
+from sqlmodel import Session, select
 
 from modelctl_core.models.universal_model import UniversalModel
 
 
 class ModelRepository:
-    def __init__(
-        self,
-        engine,
-    ):
-
+    def __init__(self, engine):
         self.engine = engine
 
-    def save_many(
-        self,
-        models: list[UniversalModel],
-    ):
-
+    def save_many(self, models: list[UniversalModel]):
         with Session(self.engine) as session:
             for model in models:
                 existing = session.exec(
@@ -27,41 +18,26 @@ class ModelRepository:
 
                 if existing:
                     existing.name = model.name
-
                     existing.context_length = model.context_length
-
                     existing.updated_at = datetime.now(UTC)
-
                 else:
                     session.add(model)
 
             session.commit()
 
-    def list(
-        self,
-    ):
-
+    def list(self):
         with Session(self.engine) as session:
             return session.exec(
                 select(UniversalModel).order_by(UniversalModel.provider, UniversalModel.model_id)
             ).all()
 
-    def search(
-        self,
-        keyword: str,
-    ):
-
+    def search(self, keyword: str):
         with Session(self.engine) as session:
             return session.exec(
                 select(UniversalModel).where(UniversalModel.model_id.contains(keyword))
             ).all()
 
-    def favorite(
-        self,
-        model_id: str,
-        value: bool,
-    ):
-
+    def favorite(self, model_id: str, value: bool):
         with Session(self.engine) as session:
             model = session.exec(
                 select(UniversalModel).where(UniversalModel.model_id == model_id)
@@ -69,12 +45,9 @@ class ModelRepository:
 
             if model:
                 model.favorite = value
-
                 session.add(model)
-
                 session.commit()
 
     def favorites(self):
-
         with Session(self.engine) as session:
-            return session.exec(select(UniversalModel).where(UniversalModel.favorite == True)).all()
+            return session.exec(select(UniversalModel).where(UniversalModel.favorite)).all()
