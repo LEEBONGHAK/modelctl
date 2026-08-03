20b4b6c13f6a914157562a847c597c020f9435c0
bolee <dlqhdgkr456@gmail.com>
2026-08-03 11:17:23 +0900

    feat(cli): register launcher management commands

--- apps/modelctl/src/modelctl_cli/main.py
+++ apps/modelctl/src/modelctl_cli/main.py
@@ -2,6 +2,7 @@ import typer
 
 from modelctl_cli.commands.auth import auth_app
 from modelctl_cli.commands.config import config_app
+from modelctl_cli.commands.launchers import launchers_app
 from modelctl_cli.commands.models import models_app
 from modelctl_cli.commands.plugins import plugins_app
 from modelctl_cli.commands.root import register_root_commands
@@ -16,6 +17,7 @@ app = typer.Typer(
 register_root_commands(app)
 
 app.add_typer(config_app, name="config")
+app.add_typer(launchers_app, name="launchers")
 app.add_typer(plugins_app, name="plugins")
 app.add_typer(models_app, name="models")
 app.add_typer(auth_app, name="auth")
